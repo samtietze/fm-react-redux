0a99d410abb3f4a0b7695f8b7ac841425a7d2965
Sam <sam.tietze@spr.com>
2018-07-17 18:59:29 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+  <div className="app">
+    <div className="landing">
+      <h1>
+        svideo
+      </h1>
+      <input type="text" placeholder="Search" />
+      <a href="/">
+        browse all
+      </a>
+    </div>
+  </div>
+);
+
+export default App;
