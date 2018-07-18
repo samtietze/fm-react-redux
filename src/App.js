844740e0b6c37287cb424881e2d44cb4115fe366
Sam <sam.tietze@spr.com>
2018-07-17 19:33:33 -0500

    Add Search.js file with bare Search component; include /search route in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { HashRouter, Route } from 'react-router-dom';
 import Landing from './Landing';
+import Search from './Search';
 import './App.css';
 
 const App = () => (
   <HashRouter>
     <div className="app">
       <Route exact path="/" component={Landing} />
+      <Route path="/search" component={Search} />
     </div>
   </HashRouter>
 );
