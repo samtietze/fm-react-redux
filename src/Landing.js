40e4f9a3bd225ad1898bcb94e7cad8132c87f10f
Sam <sam.tietze@spr.com>
2018-07-17 19:35:55 -0500

    Replace a tag with Link

--- src/Landing.js
+++ src/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => (
   <div className="landing">
@@ -6,9 +7,9 @@ const Landing = () => (
       svideo
     </h1>
     <input type="text" placeholder="Search" />
-    <a href="/">
-      browse all
-    </a>
+    <Link to="/search">
+      browse
+    </Link>
   </div>
 );
 
