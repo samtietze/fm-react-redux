6721540f54b56de072d4713ba2fe2f1dbd9f88c3
Sam <sam.tietze@spr.com>
2018-07-18 16:27:32 -0500

    Add markup to display component data

--- src/Search.js
+++ src/Search.js
@@ -17,7 +17,10 @@ const Search = () => (
   <div className="search">
     {preload.shows.map(show => (
       <div className="show-card">
-        <img src={`/public/img/posters/${show.poster}`} alt={`${show.title} Show Poster`} />
+        <img src={`/posters/${show.poster}`} alt={`${show.title} Show Poster`} />
+        <h3>{show.title}</h3>
+        <h4>({show.year})</h4>
+        <p>{show.description}</p>
       </div>
     ))}
   </div>
