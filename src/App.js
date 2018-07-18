4276386ab31c6811b7653351aaecef18cd16e44b
Sam <sam.tietze@spr.com>
2018-07-18 14:09:57 -0500

    Implement BrowserRouter instead of Hash

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Landing from './Landing';
 import Search from './Search';
 import './App.css';
 
 const App = () => (
-  <HashRouter>
+  <BrowserRouter>
     <div className="app">
       <Route exact path="/" component={Landing} />
       <Route path="/search" component={Search} />
     </div>
-  </HashRouter>
+  </BrowserRouter>
 );
 
 export default App;
