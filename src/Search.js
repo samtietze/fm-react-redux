0712ca4cce258210446f11da0acb4ae706ed784e
Sam <sam.tietze@spr.com>
2018-07-18 14:57:28 -0500

    Comment out study notes; add tutorial files for poster images and loader

--- src/Search.js
+++ src/Search.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import preload from './data';
 
+// Easy way to dump a bunch of data to the dom to test
+// const Search = () => (
+//   <div className="search">
+//     <pre>
+//       <code>
+//         {JSON.stringify(preload, null, 4)}
+//       </code>
+//     </pre>
+//   </div>
+// );
+
+// Transform payload (array) to array of React components
 const Search = () => (
   <div className="search">
-    <pre>
-      <code>
-        {JSON.stringify(preload, null, 4)}
-      </code>
-    </pre>
+    {preload.shows.map(show => (
+      <div className="show-card">
+        <img src={`${show.imageUrl}`} alt={`${show.title} Show Poster`} />
+      </div>
+    ))}
   </div>
 );
 
