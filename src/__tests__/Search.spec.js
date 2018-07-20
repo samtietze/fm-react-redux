11ff02171fbebf66b101088053bc393d0690a663
Sam <sam.tietze@spr.com>
2018-07-20 11:40:04 -0500

    add three quick tests for Search component

--- src/__tests__/Search.spec.js
+++ src/__tests__/Search.spec.js
@@ -1,16 +1,32 @@
 import React from 'react';
-// import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
+import preload from '../data.json';
 import Search from '../Search';
+import ShowCard from '../ShowCard';
 
 // This will also inadvertently test children. Is not ideal.
+// import renderer from 'react-test-renderer';
 // test('Search renders correctly', () => {
 //   const component = renderer.create(<Search />);
 //   const tree = component.toJSON();
 //   expect(tree).toMatchSnapshot();
 // });
-
-test('Search renders correctly', () => {
+it('Search renders correctly', () => {
   const component = shallow(<Search />);
   expect(component).toMatchSnapshot();
+});
+
+it('Search should render correct amount of shows', () => {
+  const component = shallow(<Search />);
+  expect(component.find(ShowCard).length).toEqual(preload.shows.length);
+});
+
+it('Search should render correct amount of shows based on search term', () => {
+  const searchWord = 'black';
+  const component = shallow(<Search />);
+  component.find('input').simulate('change', { target: { value: searchWord } });
+  // including logic like this is usually bad practice, because it doesn't effectively test it!
+  const showCount = preload.shows.filter(show => `${show.title} ${show.description}`
+    .toUpperCase().indexOf(searchWord.toUpperCase()) >= 0).length;
+  expect(component.find(ShowCard).length).toEqual(showCount);
 });
