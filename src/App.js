644fa322e4d705c5711fdeafef866e6008885d11
Sam <sam.tietze@spr.com>
2018-07-23 15:55:44 -0500

    Add typing to props

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import type { Match } from 'react-router-dom';
 import Landing from './Landing';
 import Search from './Search';
 import Details from './Details';
@@ -22,9 +23,9 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route path="/search" component={Search} />
-        <Route 
+        <Route
           path="/details/:id"
-          component={(props) => {
+          component={(props: { match: Match }) => {
             const selectedShow = preload.shows.find(show => props.match.params.id === show.imdbID);
             return <Details show={selectedShow} {...props} />;
           }}
