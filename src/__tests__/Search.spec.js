dde2298bd6d284ba68364cb2ea2971619b76f344
Sam <sam.tietze@spr.com>
2018-07-23 16:20:51 -0500

    Add shows props to shallow component sim in Search test

--- src/__tests__/Search.spec.js
+++ src/__tests__/Search.spec.js
@@ -12,19 +12,19 @@ import ShowCard from '../ShowCard';
 //   expect(tree).toMatchSnapshot();
 // });
 it('Search renders correctly', () => {
-  const component = shallow(<Search />);
+  const component = shallow(<Search shows={preload.shows} />);
   expect(component).toMatchSnapshot();
 });
 
 it('Search should render correct amount of shows', () => {
-  const component = shallow(<Search />);
+  const component = shallow(<Search shows={preload.shows} />);
   expect(component.find(ShowCard).length).toEqual(preload.shows.length);
 });
 
 it('Search should render correct amount of shows based on search term', () => {
   const searchWord = 'black';
-  const component = shallow(<Search />);
-  component.find('input').simulate('change', { target: { value: searchWord } });
+  const component = shallow(<Search shows={preload.shows} />);
+  component.find('input').simulate('change', { currentTarget: { value: searchWord } });
   // including logic like this is usually bad practice, because it doesn't effectively test it!
   const showCount = preload.shows.filter(show => `${show.title} ${show.description}`
     .toUpperCase().indexOf(searchWord.toUpperCase()) >= 0).length;
