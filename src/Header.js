5eafc1d6bc9a882e2205199643dc251812e4b7db
Sam <sam.tietze@spr.com>
2018-07-23 17:29:18 -0500

    replace header markup in Details with Header component; add conditional rendering to Header component to display 'back' button in Details screen

---
+++ src/Header.js
@@ -0,0 +1,33 @@
+// @flow
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = (props: { showSearch?: boolean }) => {
+  let utilSpace;
+  if (props.showSearch) {
+    utilSpace = <h1>lol</h1>;
+  } else {
+    utilSpace = (
+      <h2>
+        <Link to="/search">
+          Back
+        </Link>
+      </h2>
+    );
+  }
+  return (
+    <header>
+      <h1>
+        svideo
+      </h1>
+      {utilSpace}
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  showSearch: false,
+};
+
+export default Header;
