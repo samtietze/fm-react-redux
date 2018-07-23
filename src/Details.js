33903edfaace8a449b96089a9ceefa4b8673fe8e
Sam <sam.tietze@spr.com>
2018-07-23 16:38:38 -0500

    Run markup through linter; revise note at top; change year to be more lint-friendly

--- src/Details.js
+++ src/Details.js
@@ -2,31 +2,45 @@
 
 import React from 'react';
 
-{/* This is an easy way to check out the props at this component's branch */}
-{/* <h1><pre><code>{JSON.stringify(props, null, 4)}</code></pre></h1> */}
+// This is an easy way to check out the props at this component's branch:
+//  <h1>
+//    <pre><code>{JSON.stringify(props, null, 4)}</code></pre>
+//  </h1>
 
 const Details = (props: { show: Show }) => {
-  const { title, description, year, poster, trailer } = props.show;
-  <div className="details">
-    <header>
-      <h1>svideo</h1>
-    </header>
-    <section>
-      <h1>{title}</h1>
-      <h2>({year})</h2>
-      <img src={`/public/posters/${poster}`} alt={`Poster for ${title}`} />
-      <p>{description}</p>
-    </section>
-    <div>
-      <iframe 
-        src={`https://www.youtube-nocookie.com/embed/${trailer}
+  const {
+    title, description, year, poster, trailer,
+  } = props.show;
+  return (
+    <div className="details">
+      <header>
+        <h1>
+svideo
+        </h1>
+      </header>
+      <section>
+        <h1>
+          {title}
+        </h1>
+        <h2>
+          {`(${year})`}
+        </h2>
+        <img src={`/public/posters/${poster}`} alt={`Poster for ${title}`} />
+        <p>
+          {description}
+        </p>
+      </section>
+      <div>
+        <iframe
+          src={`https://www.youtube-nocookie.com/embed/${trailer}
           ?rel=0&amp;controls=0&amp;showinfo=0`}
-          frameborder="0"
+          frameBorder="0"
           allowFullscreen
           title={`Trailer for ${title}`}
-      />
+        />
+      </div>
     </div>
-  </div>
+  );
 };
 
 export default Details;
