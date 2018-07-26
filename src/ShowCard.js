4a37382daa8f3d94dd3e1327da28dfdc47b8e12d
Sam <sam.tietze@spr.com>
2018-07-26 15:52:25 -0500

    Add some styling to clean the Search component up via ShowCard components

--- src/ShowCard.js
+++ src/ShowCard.js
@@ -4,19 +4,28 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-const Wrapper = styled.div`
-  width: 32%;
+const Wrapper = styled(Link)`
+  flex: 1 1 400px;
+  display: flex;
+  flex-flow: row no wrap
   border: 2px solid #333;
   border-radius: 4px;
   margin-bottom: 25px;
   padding-right: 10px;
   overflow: hidden;
+  color: black;
+  text-decoration: none;
 `;
 
 const Image = styled.img`
   width: 46%;
+  height: 100%;
   float: left;
   margin-right: 10px;
+`;
+
+const Pgraph = styled.p`
+  // font-size: 1vw;
 `;
 
 const ShowCard = (props: {
@@ -29,20 +38,20 @@ const ShowCard = (props: {
   }}) => {
   const { show } = props;
   return (
-    <Link to={`/details/${show.imdbID}`}>
-      <Wrapper>
-        <Image src={`/posters/${show.poster}`} alt={`${show.title} Show Poster`} />
+    <Wrapper to={`/details/${show.imdbID}`}>
+      <Image src={`/posters/${show.poster}`} alt={`${show.title} Show Poster`} />
+      <div>
         <h3>
           {show.title}
         </h3>
         <h4>
           {`(${show.year})`}
         </h4>
-        <p>
+        <Pgraph>
           {show.description}
-        </p>
-      </Wrapper>
-    </Link>
+        </Pgraph>
+      </div>
+    </Wrapper>
   );
 };
 
