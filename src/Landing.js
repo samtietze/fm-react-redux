c9565f32701e8aec2408e01c49ebecaeeb4fad41
Sam <sam.tietze@spr.com>
2018-07-30 12:16:22 -0500

    Pull props typing up out of class definition; pass Props to Landing class; implement goToSearch method to prevent form submission and add /search to history object; update SyntheticEvent to SyntheticKeyboardEvent<HTMLInputElement> along with event.currentTarget.value

--- src/Landing.js
+++ src/Landing.js
@@ -1,26 +1,51 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import type { RouterHistory } from 'react-router-dom';
 import { setSearchTerm } from './actionCreators';
 
-const Landing = (props: { searchTerm: string, handleSearchTermChange: Function }) => (
-  <div className="landing">
-    <h1>
-      svideo
-    </h1>
-    <input onChange={props.handleSearchTermChange} value={props.searchTerm} type="text" placeholder="Search" />
-    <Link to="/search">
-      browse
-    </Link>
-  </div>
-);
+type Props = {
+  searchTerm: string,
+  handleSearchTermChange: Function,
+  history: RouterHistory,
+};
+
+class Landing extends Component<Props> {
+  // This component has access to the history object by
+  // default (no need to opt-in), because it exists as a Route.
+  goToSearch = (event: SyntheticKeyboardEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    this.props.history.push('/search');
+  }
+
+  render() {
+    return (
+      <div className="landing">
+        <h1>
+          svideo
+        </h1>
+        <form onSubmit={this.goToSearch}>
+          <input
+            onChange={this.props.handleSearchTermChange}
+            value={this.props.searchTerm}
+            type="text"
+            placeholder="Search"
+          />
+        </form>
+        <Link to="/search">
+          browse
+        </Link>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => ({ searchTerm: state.searchTerm });
 const mapDispatchToProps = (dispatch: Function) => ({
   handleSearchTermChange(event) {
-    dispatch(setSearchTerm(event.target.value));
+    dispatch(setSearchTerm(event.currentTarget.value));
   },
 });
 
