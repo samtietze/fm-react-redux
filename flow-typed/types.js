1943ae8e380cb38bb713e4d2c7478ce0c26be773
Sam <sam.tietze@spr.com>
2018-07-30 13:11:33 -0500

    Revise reducers to use combineReducers; set default search term in searchTerm reducer to be empty string

--- flow-typed/types.js
+++ flow-typed/types.js
@@ -12,9 +12,10 @@ export type Show = {
 // an enumerated type:
 declare type ActionType = 'SET_SEARCH_TERM'
 
-declare type ActionT<A: ActionType, P> {
+// Pipes are used for engineered types
+declare type ActionT<A: ActionType, P> = {|
   type: A,
   payload: P
-}
+|}
 
 export type Action = ActionT<'SET_SEARCH_TERM', string>;
--- src/reducers.js
+++ src/reducers.js
@@ -3,14 +3,15 @@
 import { combineReducers } from 'redux';
 import { SET_SEARCH_TERM } from './actions';
 
-const DEFAULT_STATE = {
-  searchTerm: '',
-};
-
 // Use Object.assign() for a couple of reasons: do. not. modify. the original state object.
 // Object.assign() will create a new object with the state and action. All previous state
 // gets merged into the new object, and the action will overwrite whatever state is in there.
-const setSearchTerm = (state, action) => Object.assign({}, state, { searchTerm: action.payload });
+const searchTerm = (state = '', action: Action) => {
+  if (action.type === SET_SEARCH_TERM) {
+    return action.payload;
+  }
+  return state;
+};
 
 // This is a 'flux standard action':
 // {
@@ -23,15 +24,18 @@ const setSearchTerm = (state, action) => Object.assign({}, state, { searchTerm: 
 // }
 
 // The rootReducer can be created by combineReducers function from Redux
-// to create the switch case for all of your actions
+// to create the switch case for all of your actions. This object's
+// searchTerm: searchTerm can be written as just 'searchTerm'; it's
+// syntactic sugar from ES6. Not sure why it didn't work in JSX tags.
+const rootReducer = combineReducers({ searchTerm });
 // without Flow:
-const rootReducer = (state = DEFAULT_STATE, action) => {
-  switch (action.type) {
-    case SET_SEARCH_TERM:
-      return setSearchTerm(state, action);
-    default:
-      return state;
-  }
-};
+// const rootReducer = (state = DEFAULT_STATE, action) => {
+//   switch (action.type) {
+//     case SET_SEARCH_TERM:
+//       return setSearchTerm(state, action);
+//     default:
+//       return state;
+//   }
+// };
 
 export default rootReducer;
