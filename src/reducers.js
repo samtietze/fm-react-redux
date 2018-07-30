4ef7d115cb1ede5a6e0685780286ec8541efe4d2
Sam <sam.tietze@spr.com>
2018-07-30 09:52:44 -0500

    Add quick note

--- src/reducers.js
+++ src/reducers.js
@@ -2,6 +2,16 @@ const DEFAULT_STATE = {
   searchTerm: '',
 };
 
+// This is a 'flux standard action':
+// {
+//   type: string,
+//   payload: ,
+//   error: Error,
+//   metadata: {
+//     object of other metadata to include with action
+//   }
+// }
+
 // without Flow:
 const rootReducer = (state = DEFAULT_STATE, action) => {
   switch (action.type) {
