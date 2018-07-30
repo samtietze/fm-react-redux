490036a7f2d14f2b49cd1cac39f7543e76e5f452
Sam <sam.tietze@spr.com>
2018-07-30 10:39:02 -0500

    Add redux connector to Landing

--- src/Landing.js
+++ src/Landing.js
@@ -1,6 +1,7 @@
 // @flow
 
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 const Landing = () => (
@@ -15,4 +16,9 @@ const Landing = () => (
   </div>
 );
 
-export default Landing;
+const mapStateToProps = state => ({ searchTerm: state.searchTerm });
+
+// The connect function pulls out just what it needs from the
+// state object (see mapStateToProps object above), and then it injects
+// that state as props to the following exported component (e.g. Landing);
+export default connect(mapStateToProps)(Landing);
