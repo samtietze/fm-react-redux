43b091d3e29a5819707c0da5916edd0b3216b3be
Sam <sam.tietze@spr.com>
2018-07-30 09:59:03 -0500

    Add store.js, actions.js, and actionCreators.js with minimal exports

---
+++ src/actions.js
@@ -0,0 +1,3 @@
+// This method of listing actions as variables instead of referencing
+// SET_SEARCH_TERM from multiple locations, just this file will be used.
+export const SET_SEARCH_TERM = 'SET_SEARCH_TERM';
